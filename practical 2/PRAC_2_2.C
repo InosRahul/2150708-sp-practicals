305e96ccc1b5ba713db4ce6055d04cb3fe00f849
Rahul Soni <rahulsoni2225@gmail.com>
2018-07-15 23:15:03 +0530

    added footer to all docs

--- practical 2/PRAC_2_2.C
+++ practical 2/PRAC_2_2.C
@@ -1,15 +1,11 @@
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
-
 void main(){
-
 	char ch;
 	FILE *fp;
 	clrscr();
-
 	fp=fopen("name.txt","w");
-
 	printf("Enter string till $");
 	while(1){
 		scanf("%c",&ch);
@@ -19,22 +15,18 @@ void main(){
 	}
 	fclose(fp);
 	fp=fopen("name.txt","r");
-
 	printf("\nContent of the name.txt file is\n");
 	printf("String\n");
 	printf("----------\n");
 	do{
 		ch = fgetc(fp);
 		putchar(ch);
-
 	}
 	while(ch != EOF);
-
 	fcloseall();
 	getch();
 
 }
-
 /*
 Output
 
@@ -43,6 +35,6 @@ Enter string till $hello world is too common$
 Content of the name.txt file is                                                 
 String                                                                          
 ----------                                                                      
-hello world is too commonÿ                                                      
+hello world is too common                                                      
 
 */
--- practical 2/SP_133_2.C
+++ practical 2/SP_133_2.C
@@ -1,62 +1,44 @@
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
-
 void main(){
-
 FILE *fp,*o1,*e1;
 int numbers,n,i;
 clrscr();
 printf("Enter n");
 scanf("%d",&n);
-
 fp=fopen("Numbers.txt","w");
 for(i=1;i<=n;i++){
-
 	printf("\n Enter numbers:");
-
 	scanf("%d",&numbers);
 	putw(numbers,fp);
 }
 fclose(fp);
-
-
-
 fp = fopen("Numbers.txt","r");
 printf("\nNumbers are:");
 	while((numbers = getw(fp)) != EOF)
 	printf(" %4d",numbers);
-
 fp = fopen("Numbers.txt","r");
 o1 = fopen("Odd.txt","w");
 e1 = fopen("Even.txt","w");
-
 while((numbers = getw(fp)) != EOF){
 	if(numbers %2 == 0)
 		putw(numbers, e1);
 	else
 		putw(numbers, o1);
-
-}
+	}
 fcloseall();
-
 o1 = fopen("Odd.txt","r");
 e1 = fopen("Even.txt","r");
-
 	printf("\n Odd numbers are:");
 	while((numbers = getw(o1)) != EOF)
 	printf("%4d",numbers);
-
 	printf("\n Even numbers are:");
 	while((numbers = getw(e1)) != EOF)
 	printf("%4d",numbers);
-
 fcloseall();
-
 getch();
-
 }
-
 /*
 Output
 Enter n5
